5c943ee9c1c53afb8cddf631691031f15fc26638
Masahiro Sakai <masahiro.sakai@gmail.com>
2011-06-26 14:17:36 +0900

    adding isInteger :: RealFrac a => a -> Bool and fracPart :: RealFrac a => a -> a

--- Util.hs
+++ Util.hs
@@ -4,3 +4,9 @@ combineMaybe :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
 combineMaybe _ Nothing y = y
 combineMaybe _ x Nothing = x
 combineMaybe f (Just x) (Just y) = Just (f x y)
+
+isInteger :: RealFrac a => a -> Bool
+isInteger x = fromInteger (round x) == x
+
+fracPart :: RealFrac a => a -> a
+fracPart x = x - fromInteger (floor x)
