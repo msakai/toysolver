99afdd09d80bd4e63f82c8348c8d4eda0dc32a64
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-04-19 23:30:51 +0900

    adopt test-framework-th

--- src/TestSAT.hs
+++ src/TestSAT.hs
@@ -1,14 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
 import Control.Monad
 import Test.HUnit hiding (Test)
 import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.TH
 import Test.Framework.Providers.HUnit
 import SAT
 
 -- should be SAT
-test1 :: IO ()
-test1 = do
+case_solve_SAT :: IO ()
+case_solve_SAT = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -19,8 +21,8 @@ test1 = do
   assertEqual "test1" True ret
 
 -- shuld be UNSAT
-test2 :: IO ()
-test2 = do
+case_solve_UNSAT :: IO ()
+case_solve_UNSAT = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -32,18 +34,18 @@ test2 = do
   assertEqual "test2" False ret
 
 -- top level でいきなり矛盾
-test3 :: IO ()
-test3 = do
+case_root_inconsistent :: IO ()
+case_root_inconsistent = do
   solver <- newSolver
   x1 <- newVar solver
   addClause solver [literal x1 True]
   addClause solver [literal x1 False]
   ret <- solve solver -- unsat
-  assertEqual "test3" False ret  
+  ret @?= False
 
 -- incremental に制約を追加
-test4 :: IO ()
-test4 = do
+case_incremental_solving :: IO ()
+case_incremental_solving = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -51,32 +53,47 @@ test4 = do
   addClause solver [literal x1 True,  literal x2 False] -- x1 or not x2
   addClause solver [literal x1 False, literal x2 False] -- not x1 or not x2
   ret <- solve solver -- sat
-  assertEqual "test4a" True ret
+  ret @?= True
 
   addClause solver [literal x1 False, literal x2 True]  -- not x1 or x2
   ret <- solve solver -- unsat
-  assertEqual "test4a" False ret
+  ret @?= False
+
+-- 制約なし
+case_empty_constraint :: IO ()
+case_empty_constraint = do
+  solver <- newSolver
+  ret <- solve solver
+  ret @?= True
+
+-- 空の節
+case_empty_claue :: IO ()
+case_empty_claue = do
+  solver <- newSolver
+  addClause solver []
+  ret <- solve solver
+  ret @?= False
 
 -- 自明に真な節
-test5 :: IO ()
-test5 = do
+case_excluded_middle_claue :: IO ()
+case_excluded_middle_claue = do
   solver <- newSolver
   x1 <- newVar solver
   addClause solver [x1, -x1] -- x1 or not x1
   ret <- solve solver
-  assertEqual "test5" True ret
+  ret @?= True
 
 -- 冗長な節
-test6 :: IO ()
-test6 = do
+case_redundant_clause :: IO ()
+case_redundant_clause = do
   solver <- newSolver
   x1 <- newVar solver
   addClause solver [x1,x1] -- x1 or x1
   ret <- solve solver
-  assertEqual "test6" True ret
+  ret @?= True
 
-testInstantiateClause :: IO ()
-testInstantiateClause = do
+case_instantiateClause :: IO ()
+case_instantiateClause = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -84,10 +101,10 @@ testInstantiateClause = do
   addClause solver [x1,x2]
   addClause solver [-x1,x2]
   ret <- solve solver
-  assertEqual "InstantiateClause" True ret
+  ret @?= True
 
-testInstantiateAtLeast :: IO ()
-testInstantiateAtLeast = do
+case_instantiateAtLeast :: IO ()
+case_instantiateAtLeast = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -97,14 +114,39 @@ testInstantiateAtLeast = do
 
   addAtLeast solver [x1,x2,x3,x4] 2
   ret <- solve solver
-  assertEqual "testInstantiateClause1" True ret
+  ret @?= True
 
   addAtLeast solver [-x1,-x2,-x3,-x4] 2
   ret <- solve solver
-  assertEqual "testInstantiateClause2" True ret
+  ret @?= True
 
-testAtLeast1 :: IO ()
-testAtLeast1 = do
+case_inconsistent_AtLeast :: IO ()
+case_inconsistent_AtLeast = do
+  solver <- newSolver
+  x1 <- newVar solver
+  x2 <- newVar solver
+  addAtLeast solver [x1,x2] 3
+  ret <- solve solver -- unsat
+  ret @?= False
+
+case_trivial_AtLeast :: IO ()
+case_trivial_AtLeast = do
+  solver <- newSolver
+  x1 <- newVar solver
+  x2 <- newVar solver
+  addAtLeast solver [x1,x2] 0
+  ret <- solve solver
+  ret @?= True
+
+  solver <- newSolver
+  x1 <- newVar solver
+  x2 <- newVar solver
+  addAtLeast solver [x1,x2] (-1)
+  ret <- solve solver
+  ret @?= True
+
+case_AtLeast_1 :: IO ()
+case_AtLeast_1 = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -112,10 +154,10 @@ testAtLeast1 = do
   addAtLeast solver [x1,x2,x3] 2
   addAtLeast solver [-x1,-x2,-x3] 2
   ret <- solve solver -- unsat
-  assertEqual "testAtLeast1" False ret
+  ret @?= False
 
-testAtLeast2 :: IO ()
-testAtLeast2 = do
+case_AtLeast_2 :: IO ()
+case_AtLeast_2 = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -125,21 +167,21 @@ testAtLeast2 = do
   addClause solver [-x1,-x2]
   addClause solver [-x1,-x3]
   ret <- solve solver
-  assertEqual "testAtLeast2" True ret
+  ret @?= True
 
-testAtLeast3 :: IO ()
-testAtLeast3 = do
+case_AtLeast_3 :: IO ()
+case_AtLeast_3 = do
   forM_ [(-1) .. 3] $ \n -> do
     solver <- newSolver
     x1 <- newVar solver
     x2 <- newVar solver
     addAtLeast solver [x1,x2] n
     ret <- solve solver
-    assertEqual ("testAtLeast3_" ++ show n) (n <= 2) ret
+    assertEqual ("case_AtLeast3_" ++ show n) (n <= 2) ret
 
 -- from http://www.cril.univ-artois.fr/PB11/format.pdf
-testPB1 :: IO ()
-testPB1 = do
+case_PB_sample1 :: IO ()
+case_PB_sample1 = do
   solver <- newSolver
 
   x1 <- newVar solver
@@ -154,11 +196,11 @@ testPB1 = do
   addPBExactly solver [(2,x2),(3,x4),(2,x1),(3,x5)] 5
 
   ret <- solve solver
-  assertEqual "testPB1" True ret
+  ret @?= True
 
 -- 一部の変数を否定に置き換えたもの
-testPB2 :: IO ()
-testPB2 = do
+case_PB_sample1' :: IO ()
+case_PB_sample1' = do
   solver <- newSolver
 
   x1 <- newVar solver
@@ -173,30 +215,30 @@ testPB2 = do
   addPBExactly solver [(2,-x2),(3,-x4),(2,x1),(3,x5)] 5
 
   ret <- solve solver
-  assertEqual "testPB2" True ret
+  ret @?= True
 
 -- いきなり矛盾したPB制約
-testPB3 :: IO ()
-testPB3 = do
+case_root_inconsistent_PB :: IO ()
+case_root_inconsistent_PB = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
   addPBAtLeast solver [(2,x1),(3,x2)] 6
   ret <- solve solver
-  assertEqual "testPB3" False ret
+  ret @?= False
 
-testPB4 :: IO ()
-testPB4 = do
+case_pb_propagate :: IO ()
+case_pb_propagate = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
   addPBAtLeast solver [(1,x1),(3,x2)] 3
   addClause solver [-x1]
   ret <- solve solver
-  assertEqual "testPB4" True ret
+  ret @?= True
 
-testAssumption :: IO ()
-testAssumption = do
+case_solveWith_1 :: IO ()
+case_solveWith_1 = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
@@ -207,58 +249,30 @@ testAssumption = do
   addClause solver [-x3, -x1, x2] -- not x3 or not x1 or x2
 
   ret <- solve solver -- sat
-  assertEqual "testAssumpttion1" True ret
+  ret @?= True
 
   ret <- solveWith solver [x3] -- unsat
-  assertEqual "testAssumpttion2" False ret  
+  ret @?= False
 
   ret <- solve solver -- sat
-  assertEqual "testAssumpttion3" True ret
+  ret @?= True
 
-
-testAssumption2 :: IO ()
-testAssumption2 = do
+case_solveWith_2 :: IO ()
+case_solveWith_2 = do
   solver <- newSolver
   x1 <- newVar solver
   x2 <- newVar solver
-  addClause solver [x1]      -- x1
   addClause solver [-x1, x2] -- -x1 or x2
+  addClause solver [x1]      -- x1
+
   ret <- solveWith solver [x2]
-  assertEqual "already assigned vairable is assumed" True ret
+  ret @?= True
+
+  ret <- solveWith solver [-x2]
+  ret @?= False
 
 ------------------------------------------------------------------------
 -- Test harness
 
 main :: IO ()
-main = defaultMain tests
-
-tests :: [Test]
-tests =
-    [ testCase "test1" test1
-    , testCase "test2" test2
-    , testCase "test3" test3
-    , testCase "test4" test4
-    , testCase "test5" test5
-    , testCase "test6" test6
-    , testCase "testInstantiateClause" testInstantiateClause
-    , testCase "testInstantiateAtLeast" testInstantiateAtLeast
-    , testCase "testAtLeast1" testAtLeast1
-    , testCase "testAtLeast2" testAtLeast2
-    , testCase "testAtLeast3" testAtLeast3
-    , testCase "testPB1" testPB1
-    , testCase "testPB2" testPB2
-    , testCase "testPB3" testPB3
-    , testCase "testPB4" testPB4
-    , testCase "testAssumption" testAssumption
-    , testCase "testAssumption2" testAssumption2
-    ]
-{-
-    [ testProperty "bernstein" pHash
-    , testGroup "text"
-      [ testProperty "text/strict" pText
-      , testProperty "text/lazy" pTextLazy
-      , testProperty "rechunk" pRechunk
-      , testProperty "text/rechunked" pLazyRechunked
-      ]
-    ]
--}
+main = $(defaultMainGenerator)
