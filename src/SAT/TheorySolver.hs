c374a6ab123a34f4e124266f403b6e6030cf9882
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-05-14 00:13:07 +0900

    add SAT.TheorySolver

---
+++ src/SAT/TheorySolver.hs
@@ -0,0 +1,25 @@
+module SAT.TheorySolver
+  ( TheorySolver (..)
+  , emptyTheory
+  ) where
+
+import SAT.Types
+
+data TheorySolver =
+  TheorySolver
+  { thAssertLit :: (Lit -> IO Bool) -> Lit -> IO Bool
+  , thCheck     :: (Lit -> IO Bool) -> IO Bool
+  , thExplain   :: IO [Lit]
+  , thPushBacktrackPoint :: IO ()
+  , thPopBacktrackPoint  :: IO ()
+  }
+
+emptyTheory :: TheorySolver
+emptyTheory =
+  TheorySolver
+  { thAssertLit = \propagate lit -> return True
+  , thCheck = \propagate -> return True
+  , thExplain = error "should not happen"
+  , thPushBacktrackPoint = return ()
+  , thPopBacktrackPoint  = return ()
+  }
