d2eb0bd7a4dd0e90acbf7405bdee6cdb77e54c94
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-06-03 21:32:56 +0900

    make buchberger to assume coefficients are Fractional

--- src/Polynomial.hs
+++ src/Polynomial.hs
@@ -27,6 +27,7 @@ module Polynomial
 
   -- * Conversion
   , var
+  , constant
   , fromMonomials
   , fromMonomial
   , terms
@@ -49,7 +50,7 @@ module Polynomial
   , buchberger
 
   -- * Multivariate division algorithm
-  , multivariateDivision
+  , reduce
 
   -- * Utility functions
   , showPoly
@@ -92,6 +93,9 @@ normalize (Polynomial m) = Polynomial (Map.filter (0/=) m)
 var :: (Eq k, Num k) => Var -> Polynomial k
 var x = Polynomial (Map.singleton (IMS.singleton x) 1)
 
+constant :: (Eq k, Num k) => k -> Polynomial k
+constant c = normalize $ Polynomial (Map.singleton IMS.empty c)
+
 fromMonomials :: (Eq k, Num k) => [Monomial k] -> Polynomial k
 fromMonomials = normalize . Polynomial . Map.fromListWith (+) . map (\(c,xs) -> (xs,c))
 
@@ -125,10 +129,9 @@ deriv p x = sum $ do
     else return $ fromMonomial (c * fromIntegral n, IMS.delete x xs)
 
 showPoly :: (Eq k, Ord k, Num k, Show k) => Polynomial k -> String
-showPoly p = intercalate " " [f c xs | (c,xs) <- sortBy (flip grlex `on` snd) $ terms p]
+showPoly p = intercalate " + " [f c xs | (c,xs) <- sortBy (flip grlex `on` snd) $ terms p]
   where
-    f c xs = (if c >= 0 then "+" else "") ++ 
-             (intercalate "*" ([show c | c /= 1] ++ [g x n | (x,n) <- IMS.toOccurList xs]))
+    f c xs = (intercalate "*" ([showsPrec 8 c "" | c /= 1] ++ [g x n | (x,n) <- IMS.toOccurList xs]))
     g x 1 = "x" ++ show x
     g x n = "x" ++ show x ++ "^" ++ show n
 
@@ -154,6 +157,14 @@ test_lex_3 = lex (IMS.fromOccurList [(2,2)]) (IMS.fromOccurList [(1,1),(2,1)]) =
 grlex :: MonomialOrder
 grlex = (compare `on` monicMonomialDegree) `mappend` lex
 
+test_grlex = fromMonomial (leadingTerm grlex f) == x^3*y^2 &&
+             fromMonomial (leadingTerm grlex g) == 3*x^4*y
+  where
+    x = var 1
+    y = var 2
+    f, g :: Polynomial Rational
+    f = x^3*y^2 - x^2*y^3
+    g = 3*x^4*y + y^2
 
 lcmMonicMonomial :: MonicMonomial -> MonicMonomial -> MonicMonomial
 lcmMonicMonomial = IMS.maxUnion
@@ -164,17 +175,21 @@ test_lcmMonicMonomial = lcmMonicMonomial p1 p2 == IMS.fromOccurList [(1,3),(2,4)
     p1 = IMS.fromOccurList [(1,2),(2,4)]
     p2 = IMS.fromOccurList [(1,3),(2,1)]
 
-lcmMonomial :: (Eq k, Integral k) => Monomial k -> Monomial k -> Monomial k
-lcmMonomial (c1,xs1) (c2,xs2) = (lcm c1 c2, lcmMonicMonomial xs1 xs2)
+monicMonomialDivisible :: MonicMonomial -> MonicMonomial -> Bool
+monicMonomialDivisible xs1 xs2 = xs2 `IMS.isSubsetOf` xs1
 
-monomialDivisible :: (Eq k, Integral k) => Monomial k -> Monomial k -> Bool
-monomialDivisible (c1,xs1) (c2,xs2) = c1 `mod` c2 == 0 && xs2 `IMS.isSubsetOf` xs1
+monicMonomialDiv :: MonicMonomial -> MonicMonomial -> MonicMonomial
+monicMonomialDiv xs1 xs2 = xs1 `IMS.difference` xs2
 
-monomialDiv :: (Eq k, Integral k) => Monomial k -> Monomial k -> Monomial k
-monomialDiv (c1,xs1) (c2,xs2) = (c1 `div` c2, xs1 `IMS.difference` xs2)
+monomialDivisible :: Fractional k => Monomial k -> Monomial k -> Bool
+monomialDivisible (c1,xs1) (c2,xs2) = monicMonomialDivisible xs1 xs2
 
-multivariateDivision  :: (Eq k, Integral k) => MonomialOrder -> Polynomial k -> [Polynomial k] -> Polynomial k
-multivariateDivision cmp p fs = go p
+monomialDiv :: Fractional k => Monomial k -> Monomial k -> Monomial k
+monomialDiv (c1,xs1) (c2,xs2) = (c1 / c2, xs1 `monicMonomialDiv` xs2)
+
+-- | Multivariate division algorithm
+reduce  :: (Eq k, Fractional k) => MonomialOrder -> Polynomial k -> [Polynomial k] -> Polynomial k
+reduce cmp p fs = go p
   where
     ls = [(leadingTerm cmp f, f) | f <- fs]
     go g = if null xs then g else go (head xs)
@@ -186,8 +201,25 @@ multivariateDivision cmp p fs = go p
           guard $ monomialDivisible h a
           return (g - fromMonomial (monomialDiv h a) * f)
 
+spolynomial :: Fractional k => MonomialOrder -> Polynomial k -> Polynomial k -> Polynomial k
+spolynomial cmp f g =
+      fromMonomial ((1,xs) `monomialDiv` (c1,xs1)) * f
+    - fromMonomial ((1,xs) `monomialDiv` (c2,xs2)) * g
+  where
+    xs = lcmMonicMonomial xs1 xs2
+    (c1, xs1) = leadingTerm cmp f
+    (c2, xs2) = leadingTerm cmp g
 
-buchberger :: forall k. (Eq k, Integral k) => MonomialOrder -> [Polynomial k] -> [Polynomial k]
+-- http://math.rice.edu/~cbruun/vigre/vigreHW6.pdf
+test_spolynomial = spolynomial grlex f g == - x^3*y^3 - constant (1/3) * y^3 + x^2
+  where
+    x = var 1
+    y = var 2
+    f, g :: Polynomial Rational
+    f = x^3*y^2 - x^2*y^3 + x
+    g = 3*x^4*y + y^2
+
+buchberger :: forall k. (Eq k, Fractional k, Ord k) => MonomialOrder -> [Polynomial k] -> [Polynomial k]
 buchberger cmp fs = (Set.toList . go . Set.fromList) fs
   where  
     go :: Set.Set (Polynomial k) -> Set.Set (Polynomial k)
@@ -196,35 +228,37 @@ buchberger cmp fs = (Set.toList . go . Set.fromList) fs
             else go (fs `Set.union` fs2)
       where
         fs2 = Set.fromList $ do
-          let fs' = [(leadingTerm cmp f, f) | f <- Set.toList fs]
-          ((gi,fi),(gj,fj)) <- pairs fs'
+          (fi,fj) <- pairs (Set.toList fs)
           guard $ fi /= fj
-          let aij = lcmMonomial gi gj
-          let spoly = fromMonomial (aij `monomialDiv` gi) * fi - fromMonomial (aij `monomialDiv` gj) * fj
-          let p = multivariateDivision cmp spoly (Set.toList fs)
+          let spoly = spolynomial cmp fi fj
+          let p = reduce cmp spoly (Set.toList fs)
           guard $ p /= 0
           return p
 
 test_buchberger1 = buchberger lex [x^2-y, x^3-z]
   where
+    x :: Polynomial Rational
     x = var 1
     y = var 2
     z = var 3
 
 test_buchberger2 = buchberger lex [x^3-2*x*y, x^2*y-2*y^2+x]
   where
+    x :: Polynomial Rational
     x = var 1
     y = var 2
 
 -- http://www.iisdavinci.it/jeometry/buchberger.html
 test_buchberger3 = buchberger lex [x^2+2*x*y^2, x*y+2*y^3-1]
   where
+    x :: Polynomial Rational
     x = var 1
     y = var 2
 
 -- http://www.orcca.on.ca/~reid/NewWeb/DetResDes/node4.html
 test_buchberger4 = buchberger grlex [x^2+y*z-2, x*z*y^2-3, x*y+z^2-5]
   where
+    x :: Polynomial Rational
     x = var 1
     y = var 2
     z = var 3
