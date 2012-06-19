6dba33e353b4424ce4c2298d99b54c808a2d5d50
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-06-19 21:57:42 +0900

    add Data.BoolRing

---
+++ src/Data/BoolRing.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving #-}
+module Data.BoolRing
+  ( BoolRing (..)
+  ) where
+
+import Data.Ix
+import Data.Typeable
+import Data.Generics
+
+newtype BoolRing = BoolRing Bool
+  deriving (Eq, Show, Read, Enum, Ord, Ix, Data, Typeable)
+
+instance Num BoolRing where
+  BoolRing a + BoolRing b = BoolRing (a /= b) -- xor
+  BoolRing a * BoolRing b = BoolRing (a && b)
+  negate (BoolRing x) = BoolRing (not x)
+  abs x = x
+  signum _ = BoolRing True
+  fromInteger i = BoolRing (i `mod` 2 == 1)
