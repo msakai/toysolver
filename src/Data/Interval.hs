743f4c2c838701cb541c4af2a85dc8c0c4b5d9b5
Masahiro Sakai <masahiro.sakai@gmail.com>
2012-06-25 02:01:34 +0900

    fix bugs of Data.Interval

--- src/Data/Interval.hs
+++ src/Data/Interval.hs
@@ -121,7 +121,8 @@ singleton x = Interval (Just (True, x)) (Just (True, x))
 
 -- | intersection (greatest lower bounds) of two intervals
 intersection :: forall r. (Ord r, Num r) => Interval r -> Interval r -> Interval r
-intersection (Interval l1 u1) (Interval l2 u2) = interval (maxLB l1 l2) (minLB u1 u2)
+intersection i1 i2 | null i1 || null i2 = empty
+intersection (Interval l1 u1) (Interval l2 u2) = interval (maxLB l1 l2) (minUB u1 u2)
   where
     maxLB :: EndPoint r -> EndPoint r -> EndPoint r
     maxLB = combineMaybe $ \(in1,x1) (in2,x2) ->
@@ -130,9 +131,9 @@ intersection (Interval l1 u1) (Interval l2 u2) = interval (maxLB l1 l2) (minLB u
           LT -> in2
           GT -> in1
       , max x1 x2
-      )    
-    minLB :: EndPoint r -> EndPoint r -> EndPoint r
-    minLB = combineMaybe $ \(in1,x1) (in2,x2) ->
+      )
+    minUB :: EndPoint r -> EndPoint r -> EndPoint r
+    minUB = combineMaybe $ \(in1,x1) (in2,x2) ->
       ( case x1 `compare` x2 of
           EQ -> in1 && in2
           LT -> in1
@@ -250,12 +251,16 @@ tightenToInteger (Interval lb ub) = interval (fmap tightenLB lb) (fmap tightenUB
 -- Note that this instance does not satisfy algebraic laws of linear spaces.
 instance Real r => Module r (Interval r) where
   lzero = singleton 0
+  i1 .+. i2 | null i1 || null i2 = empty
   Interval lb1 ub1 .+. Interval lb2 ub2 = interval (f lb1 lb2) (f ub1 ub2)
     where
       f = liftM2 $ \(in1,x1) (in2,x2) -> (in1 && in2, x1 + x2)
-  c .*. Interval lb ub
-    | c < 0     = interval (f ub) (f lb)
-    | otherwise = interval (f lb) (f ub)
+  c .*. i | null i = i
+  c .*. Interval lb ub =
+    case compare c 0 of
+      EQ -> singleton 0
+      LT -> interval (f ub) (f lb)
+      GT -> interval (f lb) (f ub)
     where
       f Nothing = Nothing
       f (Just (incl,val)) = Just (incl, c * val)
