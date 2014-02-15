6f49a5590e239203637cc1f9219da7a8976223b4
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-02-15 12:45:51 +0900

    speed up src/pigeonhole.hs a bit

--- src/pigeonhole.hs
+++ src/pigeonhole.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Data.List
+import qualified Data.Map as Map
+import Data.Map (Map)
 import System.Environment
 import System.Exit
 import System.IO
@@ -9,15 +11,15 @@ import Text.PBFile as PBFile
 pigeonHole :: Integer -> Integer -> Formula
 pigeonHole p h = (Nothing, cs1 ++ cs2)
   where
-    vs :: [((Integer,Integer), Int)]
-    vs = zip [(i,j) | i<-[1..p], j<-[1..h]] [1..]
+    vs :: Map (Integer,Integer) Int
+    vs = Map.fromList $ zip [(i,j) | i<-[1..p], j<-[1..h]] [1..]
 
     cs1 :: [Constraint]
-    cs1 = [ ([(1,[v]) | j<-[1..h], let Just v = lookup (i,j) vs], PBFile.Ge, 1)
+    cs1 = [ ([(1,[v]) | j<-[1..h], let v = vs Map.! (i,j)], PBFile.Ge, 1)
           | i<-[1..p]
           ]
     cs2 :: [Constraint]
-    cs2 = [ ([(-1,[v]) | i<-[1..p], let Just v = lookup (i,j) vs], PBFile.Ge, -1)
+    cs2 = [ ([(-1,[v]) | i<-[1..p], let v = vs Map.! (i,j)], PBFile.Ge, -1)
           | j<-[1..h]
           ]
 
