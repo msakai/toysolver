1a94a4c5db0badb9690ef1ca34b072ecdeec3f66
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-04-06 21:50:10 +0900

    include version information of newly added dependency packages in ToySolver.Version module

--- src/ToySolver/Version.hs
+++ src/ToySolver/Version.hs
@@ -14,6 +14,9 @@ packageVersions = sort $ tail
 #ifdef VERSION_OptDir
   , ("OptDir",       VERSION_OptDir       )
 #endif
+#ifdef VERSION_algebra
+  , ("algebra",      VERSION_algebra      )
+#endif
 #ifdef VERSION_array
   , ("array",        VERSION_array        )
 #endif
@@ -35,11 +38,26 @@ packageVersions = sort $ tail
 #ifdef VERSION_filepath
   , ("filepath",     VERSION_filepath     )
 #endif
+#ifdef VERSION_finite_field
+  , ("finite-field", VERSION_finite_field )
+#endif
+#ifdef VERSION_hashable
+  , ("hashable",     VERSION_hashable     )
+#endif
 #ifdef VERSION_heaps
-  , ("heaps",        VERSION_heaps     )
+  , ("heaps",        VERSION_heaps        )
+#endif
+#ifdef VERSION_intern
+  , ("intern",       VERSION_intern       )
+#endif
+#ifdef VERSION_lattices
+  , ("lattices",     VERSION_lattices     )
 #endif
 #ifdef VERSION_mtl
   , ("mtl",          VERSION_mtl          )
+#endif
+#ifdef VERSION_multiset
+  , ("multiset",     VERSION_multiset     )
 #endif
 #ifdef VERSION_old_locale
   , ("old_locale",   VERSION_old_locale   )
@@ -50,6 +68,9 @@ packageVersions = sort $ tail
 #ifdef VERSION_parsec
   , ("parsec",       VERSION_parsec       )
 #endif
+#ifdef VERSION_prettyclass
+  , ("prettyclass",  VERSION_prettyclass  )
+#endif
 #ifdef VERSION_primes
   , ("primes",       VERSION_primes       )
 #endif
@@ -59,14 +80,23 @@ packageVersions = sort $ tail
 #ifdef VERSION_random
   , ("random",       VERSION_random       )
 #endif
+#ifdef VERSION_sign
+  , ("sign",         VERSION_sign         )
+#endif
 #ifdef VERSION_stm
   , ("stm",          VERSION_stm          )
 #endif
 #ifdef VERSION_time
   , ("time",         VERSION_time         )
 #endif
+#ifdef VERSION_type_level_numbers
+  , ("type-level-numbers", VERSION_type_level_numbers)
+#endif
 #ifdef VERSION_unbounded_delays
   , ("unbounded-delays", VERSION_unbounded_delays)
+#endif
+#ifdef VERSION_unordered_containers
+  , ("unordered-containers", VERSION_unordered_containers)
 #endif
 #ifdef VERSION_vector_space
   , ("vector-space", VERSION_vector_space)
