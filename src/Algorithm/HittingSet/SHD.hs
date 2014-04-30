b301d99bc5703f0d6303bcaabf3ab72e2bf070dc
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-05-01 01:23:00 +0900

    add wrappers for htcbdd command and shd command

---
+++ src/Algorithm/HittingSet/SHD.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -Wall #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Algorithm.HittingSet.SHD
+-- Copyright   :  (c) Masahiro Sakai 2014
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  masahiro.sakai@gmail.com
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-----------------------------------------------------------------------------
+module Algorithm.HittingSet.SHD
+  ( Options (..)
+  , Failure (..)
+  , defaultOptions
+  , minimalHittingSets
+  ) where
+
+import Control.Exception (Exception, throwIO)
+import Control.Monad
+import Data.Array.Unboxed
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.List
+import Data.Typeable
+import System.Exit
+import System.IO
+import System.IO.Temp
+import ToySolver.ProcessUtil (runProcessWithOutputCallback)
+
+type Vertex = Int
+type HyperEdge = [Vertex]
+type HittingSet = [Vertex]
+
+data Options
+  = Options
+  { optSHDCommand     :: FilePath
+  , optSHDArgs        :: [String]
+  , optOnGetLine      :: String -> IO ()
+  , optOnGetErrorLine :: String -> IO ()
+  }
+
+defaultOptions :: Options
+defaultOptions =
+  Options
+  { optSHDCommand     = "shd"
+  , optSHDArgs        = ["0"]
+  , optOnGetLine      = \_ -> return ()
+  , optOnGetErrorLine = \_ -> return ()
+  }
+
+data Failure = Failure !Int
+  deriving (Show, Typeable)
+
+instance Exception Failure
+
+minimalHittingSets :: Options -> [HyperEdge] -> IO [HittingSet]
+minimalHittingSets opt es = do
+  withSystemTempFile "shd-input.dat" $ \fname1 h1 -> do
+    forM_ es' $ \e -> do
+      hPutStrLn h1 $ intercalate " " [show (encTable IntMap.! v) | v <- IntSet.toList e]
+    hClose h1
+    withSystemTempFile "shd-out.dat" $ \fname2 h2 -> do
+      hClose h2
+      execSHD opt fname1 fname2
+      s <- readFile fname2
+      return $ map (map ((decTable !) . read) . words) $ lines s
+  where
+    es' :: [IntSet]
+    es' = map IntSet.fromList es
+
+    vs :: IntSet
+    vs = IntSet.unions es'
+
+    nv :: Int
+    nv = IntSet.size vs
+
+    encTable :: IntMap Int
+    encTable = IntMap.fromList (zip (IntSet.toList vs) [0..nv-1])
+
+    decTable :: UArray Int Int
+    decTable = array (0,nv-1) (zip [0..nv-1] (IntSet.toList vs))
+
+execSHD :: Options -> FilePath -> FilePath -> IO ()
+execSHD opt inputFile outputFile = do
+  let args = optSHDArgs opt ++ [inputFile, outputFile]
+  exitcode <- runProcessWithOutputCallback (optSHDCommand opt) args (optOnGetLine opt) (optOnGetErrorLine opt)
+  case exitcode of
+    ExitFailure n -> throwIO $ Failure n
+    ExitSuccess -> return ()
---
+++ src/ToySolver/ProcessUtil.hs
@@ -0,0 +1,70 @@
+{-# OPTIONS_GHC -Wall #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  ToySolver.ProcessUtil
+-- Copyright   :  (c) Masahiro Sakai 2014
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  masahiro.sakai@gmail.com
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-----------------------------------------------------------------------------
+module ToySolver.ProcessUtil
+  ( runProcessWithOutputCallback
+  ) where
+
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Exception (SomeException, try, mask)
+import qualified Control.Exception as C
+import Control.Monad
+import System.Exit
+import System.IO
+import System.IO.Error
+import System.Process
+
+runProcessWithOutputCallback
+  :: FilePath -- ^ Filename of the executable (see 'proc' for details)
+  -> [String] -- ^ any arguments
+  -> (String -> IO ()) -- ^ callback function which is called when a line is read from stdout
+  -> (String -> IO ()) -- ^ callback function which is called when a line is read from stderr
+  -> IO ExitCode
+runProcessWithOutputCallback cmd args putMsg putErr = do
+  (Just inh, Just outh, Just errh, processh) <- createProcess
+    (proc cmd args)
+    { std_in  = CreatePipe
+    , std_out = CreatePipe
+    , std_err = CreatePipe
+    }
+  req <- newEmptyTMVarIO
+  let f act = atomically (putTMVar req act)
+      m1 = forever (hGetLine outh >>= \s -> f (putMsg s))
+           `catchIOError` (\e -> if isEOFError e then return () else ioError e)
+      m2 = forever (hGetLine errh >>= \s -> f (putErr s))
+           `catchIOError` (\e -> if isEOFError e then return () else ioError e)
+  withForkWait m1 $ \waitOut -> do
+    withForkWait m2 $ \waitErr -> do
+      hClose inh
+      hSetBuffering outh LineBuffering
+      hSetBuffering errh LineBuffering
+      let loop = join $ atomically $ msum $
+            [ do act <- takeTMVar req
+                 return $ act >> loop
+            , do guard =<< isEmptyTMVar req
+                 waitOut
+                 waitErr
+                 return $ return ()
+            ]
+      loop
+      hClose outh
+      hClose errh
+  waitForProcess processh
+
+withForkWait :: IO () -> (STM () ->  IO a) -> IO a
+withForkWait async body = do
+  waitVar <- newEmptyTMVarIO :: IO (TMVar (Either SomeException ()))
+  mask $ \restore -> do
+    tid <- forkIO $ try (restore async) >>= \v -> atomically (putTMVar waitVar v)
+    let wait = takeTMVar waitVar >>= either throwSTM return
+    restore (body wait) `C.onException` killThread tid
