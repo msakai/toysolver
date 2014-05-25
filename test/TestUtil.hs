36518bf7f473a5c0ea9a2051a4d129076cfd02e4
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-05-25 22:55:30 +0900

    add test cases for readUnsignedInteger of ToySolver.Internal.TextUtil

--- test/TestUtil.hs
+++ test/TestUtil.hs
@@ -2,10 +2,13 @@
 module Main (main) where
 
 import Test.HUnit hiding (Test)
+import Test.QuickCheck
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.TH
+import Test.Framework.Providers.QuickCheck2
 import Test.Framework.Providers.HUnit
 import ToySolver.Internal.Util
+import ToySolver.Internal.TextUtil
 import qualified Algorithm.Knapsack as Knapsack
 
 case_showRationalAsDecimal :: IO ()
@@ -21,6 +24,14 @@ case_showRationalAsDecimal = do
   showRationalAsFiniteDecimal (-5/4) @?= Just "-1.25"
   showRationalAsFiniteDecimal (4/3)  @?= Nothing
   showRationalAsFiniteDecimal (-4/3) @?= Nothing
+
+case_readUnsignedInteger_maxBound_bug :: IO ()
+case_readUnsignedInteger_maxBound_bug =
+  readUnsignedInteger "006666666666666667" @?= 6666666666666667
+
+prop_readUnsignedInteger = 
+  forAll (choose (0, 2^128)) $ \i -> 
+    readUnsignedInteger (show i) == i
 
 case_knapsack_1 :: IO ()
 case_knapsack_1 = Knapsack.solve [(5,4), (6,5), (3,2)] 9 @?= (11, 9, [True,True,False])
