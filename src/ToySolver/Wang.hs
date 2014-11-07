4210d7f1d620af42918bf87679597e22864fc9a1
Masahiro Sakai <masahiro.sakai@gmail.com>
2014-11-08 01:44:12 +0900

    change ToySolver.Wang to use BoolExpr

--- src/ToySolver/Wang.hs
+++ src/ToySolver/Wang.hs
@@ -1,17 +1,18 @@
-module ToySolver.Wang where
+{-# OPTIONS_GHC -Wall #-}
+module ToySolver.Wang
+  ( Formula
+  , Sequent
+  , isValid
+  ) where
 
 import Control.Monad (guard,msum)
 import Data.List (intersect)
 import Data.Maybe (isJust, listToMaybe)
 
-data Formula x
-    = Atom x
-    | Not   !(Formula x)
-    | Imply !(Formula x) !(Formula x)
-    | And   !(Formula x) !(Formula x)
-    | Or    !(Formula x) !(Formula x)
-    deriving Eq
+import ToySolver.Data.Boolean
+import ToySolver.Data.BoolExpr
 
+type Formula a = BoolExpr a
 type Sequent x = ([Formula x], [Formula x])
 
 isValid :: Eq x => Sequent x -> Bool
@@ -29,16 +30,21 @@ isValid' (l,r) =
               return [(l,p:r)]
          , do (Imply p q, r) <- pick r
               return [(p:l,q:r)]
-         , do (Or p q, r) <- pick r
-              return [(l,p:q:r)]
-         , do (And p q, l) <- pick l
-              return [(p:q:l,r)]
-         , do (Or p q, l)  <- pick l
-              return [(p:l,r), (q:l,r)]
-         , do (And p q, r) <- pick r
-              return [(l,p:r), (l,q:r)]
+         , do (Or ps, r) <- pick r
+              return [(l,ps++r)]
+         , do (And ps, l) <- pick l
+              return [(ps++l,r)]
+         , do (Or ps, l)  <- pick l
+              return [(p:l,r) | p <- ps]
+         , do (And ps, r) <- pick r
+              return [(l,p:r) | p <- ps]
          , do (Imply p q, l) <- pick l
               return [(q:l,r), (l,p:r)]
+
+         , do (Equiv p q, l) <- pick l
+              return [(Imply p q : Imply q p : l, r)]
+         , do (Equiv p q, r) <- pick r
+              return [(l, Imply p q : Imply q p : r)]
          ]
        mapM_ isValid' xs
        return ()
