359958607db3c7ac30ad633646783726bf64b073
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-11-28 23:47:34 +0900

    add REPL to toysmt

--- toysmt/toysmt.hs
+++ toysmt/toysmt.hs
@@ -12,25 +12,102 @@
 -----------------------------------------------------------------------------
 module Main where
 
-import Control.Applicative
 import Control.Monad
+import Data.Default.Class
+import Data.Version
+import System.Console.GetOpt
 import System.Environment
 import System.Exit
 import System.IO
-import Text.ParserCombinators.Parsec
-import Smtlib.Parsers.CommandsParsers
+import Text.Parsec
+import Text.Parsec.String
 
+import ToySolver.Version
+
+import Smtlib.Parsers.CommandsParsers
 import Solver
+
+data Mode
+  = ModeHelp
+  | ModeVersion
+  | ModeInteractive
+  deriving (Eq, Ord, Bounded, Enum)
+
+data Options
+  = Options
+  { optMode :: Maybe Mode
+  , optInteractive :: Bool
+  }
+
+instance Default Options where
+  def =
+    Options
+    { optMode = Nothing
+    , optInteractive = False 
+    }
+
+options :: [OptDescr (Options -> Options)]
+options =
+    [ Option ['h'] ["help"]   (NoArg (\opt -> opt{ optMode = Just ModeHelp   })) "show help"
+    , Option [] ["version"]   (NoArg (\opt -> opt{ optMode = Just ModeVersion})) "show version"
+    , Option [] ["interactive"] (NoArg (\opt -> opt{ optMode = Just ModeInteractive })) "force interactive mode"
+    ]
 
 main :: IO ()
 main = do
-  [fname] <- getArgs
+  args <- getArgs
+  case getOpt Permute options args of
+    (_,_,errs@(_:_)) -> do
+      mapM_ putStrLn errs
+      exitFailure
+
+    (o,args2,[]) -> do
+      let opt = foldl (flip id) def o
+      case optMode opt of
+        Just ModeHelp -> showHelp stdout
+        Just ModeVersion -> hPutStrLn stdout (showVersion version)
+        Just ModeInteractive -> do
+          solver <- newSolver
+          mapM_ (loadFile solver) args2
+          repl solver          
+        Nothing -> do
+          solver <- newSolver
+          if null args2 then
+            repl solver
+          else
+            mapM_ (loadFile solver) args2
+
+loadFile :: Solver -> FilePath -> IO ()
+loadFile solver fname = do
   ret <- parseFromFile parseSource fname
   case ret of
     Left err -> do
       hPrint stderr err
       exitFailure
     Right source -> do
-      solver <- newSolver
       forM_ source $ \cmd -> do
         execCommand solver cmd
+
+repl :: Solver -> IO ()
+repl solver = do
+  hSetBuffering stdin LineBuffering
+  forever $ do
+    putStr "toysmt> "
+    hFlush stdout
+    s <- getLine
+    case parse (spaces >> parseCommand) "<stdin>" s of
+      Left err -> do
+        hPrint stderr err
+      Right cmd -> do
+        execCommand solver cmd
+
+showHelp :: Handle -> IO ()
+showHelp h = hPutStrLn h (usageInfo header options)
+
+header :: String
+header = unlines
+  [ "Usage:"
+  , "  toysmt [OPTION]... [file.smt2]"
+  , ""
+  , "Options:"
+  ]
