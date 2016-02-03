816766b6901dad90ce9b6230815707ad68591569
Masahiro Sakai <masahiro.sakai@gmail.com>
2016-02-03 09:15:34 +0900

    toyqbf: add a test to check when the innermost universally quantified variable

--- test/Test/QBF.hs
+++ test/Test/QBF.hs
@@ -35,6 +35,44 @@ prop_solveCEGAR = QM.monadicIO $ do
     Just ls ->
       QM.assert $ sat == evalQBFNaive' (IntMap.fromList [(abs lit, lit > 0) | lit <- IntSet.toList ls]) prefix' matrix
 
+{-
+If the innermost quantifier is a universal quantifier,
+we can remove the bound variable v from the clauses that
+do not contain v and ¬v simultaneously.
+-}
+prop_eliminate_last_universal_quantifier = QM.monadicIO $ do
+  (nv, prefix, matrix1) <- QM.pick gen
+  let (QBF.A, xs) = last prefix
+      matrix2 = [[lit | lit <- clause, abs lit `IntSet.notMember` xs] | clause <- matrix1]
+      matrix1' = andB [orB [f lit | lit <- clause] | clause <- matrix1]
+      matrix2' = andB [orB [f lit | lit <- clause] | clause <- matrix2]
+      f lit = if lit > 0
+              then BoolExpr.Atom lit
+              else notB (BoolExpr.Atom (abs lit))
+  (sat1, cert1) <- QM.run $ QBF.solveCEGAR nv prefix matrix1'
+  (sat2, cert2) <- QM.run $ QBF.solveCEGAR nv (init prefix) matrix2'
+  QM.assert $ sat1 == sat2
+  where
+    gen :: Gen (Int, QBF.Prefix, [[Int]])
+    gen = do
+      nv <- choose (0,10)
+      nc <- choose (0,50)
+      let vs = [1..nv]
+      q1 <- arbitrary
+      q2 <- arbitrary
+      vs1 <- IntSet.fromList <$> subsetof vs
+      vs2 <- IntSet.fromList <$> subsetof vs    
+      matrix <- replicateM nc $ do
+        if nv == 0 then
+          return []
+        else do
+          mapM (\v -> elements [v, -v]) =<< sublistOf vs
+      return
+        ( nv
+        , [(q1,vs1), (q2, vs2 IntSet.\\ vs1), (QBF.A, IntSet.fromList vs IntSet.\\ (vs1 `IntSet.union` vs2))]
+        , matrix
+        )
+
 -- -------------------------------------------------------------------
 
 evalQBFNaive :: QBF.Prefix -> QBF.Matrix -> Bool
@@ -58,6 +96,15 @@ subsetof xs = catMaybes <$> sequence [elements [Just x, Nothing] | x <- xs]
 
 arbitrarySmallQBF :: Gen (Int, QBF.Prefix, QBF.Matrix)
 arbitrarySmallQBF = do
+  (nv, prefix, matrix') <- arbitrarySmallQBF'
+  let f lit = if lit > 0
+              then BoolExpr.Atom lit
+              else notB (BoolExpr.Atom (abs lit))
+      matrix = andB [orB [f lit | lit <- clause] | clause <- matrix']
+  return (nv, prefix, matrix)
+
+arbitrarySmallQBF' :: Gen (Int, QBF.Prefix, [[Int]])
+arbitrarySmallQBF' = do
   nv <- choose (0,10)
   nc <- choose (0,50)
 
@@ -68,16 +115,12 @@ arbitrarySmallQBF = do
   vs1 <- IntSet.fromList <$> subsetof vs
   vs2 <- IntSet.fromList <$> subsetof vs
 
-  matrix' <- replicateM nc $ do
+  matrix <- replicateM nc $ do
     len <- choose (0,10)
     if nv == 0 then
       return []
     else
       replicateM len $ choose (-nv, nv) `suchThat` (/= 0)
-  let f lit = if lit > 0
-              then BoolExpr.Atom lit
-              else notB (BoolExpr.Atom (abs lit))
-      matrix = andB [orB [f lit | lit <- clause] | clause <- matrix']
   
   return
     ( nv
