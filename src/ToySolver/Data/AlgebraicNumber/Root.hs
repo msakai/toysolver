7aaa391bcd9fe560ba9a10bdaa5192827662dbb7
Masahiro Sakai <masahiro.sakai@gmail.com>
2016-04-08 23:49:17 +0900

    generalize ToySolver.Data.AlgebraicNumber.Root to handle fields other than Q

--- src/ToySolver/Data/AlgebraicNumber/Root.hs
+++ src/ToySolver/Data/AlgebraicNumber/Root.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types, ScopedTypeVariables #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  ToySolver.Data.AlgebraicNumber.Root
--- Copyright   :  (c) Masahiro Sakai 2012
+-- Copyright   :  (c) Masahiro Sakai 2012-2016
 -- License     :  BSD-style
 -- 
 -- Maintainer  :  masahiro.sakai@gmail.com
 -- Stability   :  provisional
--- Portability :  non-portable (Rank2Types)
+-- Portability :  non-portable (Rank2Types, ScopedTypeVariables)
 --
 -- Manipulating polynomials for corresponding operations for algebraic numbers.
 -- 
@@ -34,63 +34,65 @@ type Var = Int
   Manipulation of polynomials
 --------------------------------------------------------------------}
 
-normalizePoly :: UPolynomial Rational -> UPolynomial Rational
+normalizePoly :: (Fractional k, Eq k) => UPolynomial k -> UPolynomial k
 normalizePoly = P.toMonic P.nat
 
-rootAdd :: UPolynomial Rational -> UPolynomial Rational -> UPolynomial Rational
+rootAdd :: (Fractional k, Ord k) => UPolynomial k -> UPolynomial k -> UPolynomial k
 rootAdd p1 p2 = lift2 (+) p1 p2
 
-rootMul :: UPolynomial Rational -> UPolynomial Rational -> UPolynomial Rational
+rootMul :: (Fractional k, Ord k) => UPolynomial k -> UPolynomial k -> UPolynomial k
 rootMul p1 p2 = lift2 (*) p1 p2
 
-rootShift :: Rational -> UPolynomial Rational -> UPolynomial Rational
+rootShift :: (Fractional k, Eq k) => k -> UPolynomial k -> UPolynomial k
 rootShift 0 p = p
 rootShift r p = normalizePoly $ P.subst p (\X -> P.var X - P.constant r)
 
-rootScale :: Rational -> UPolynomial Rational -> UPolynomial Rational
+rootScale :: (Fractional k, Eq k) => k -> UPolynomial k -> UPolynomial k
 rootScale 0 p = P.var X
 rootScale r p = normalizePoly $ P.subst p (\X -> P.constant (recip r) * P.var X)
 
-rootRecip :: UPolynomial Rational -> UPolynomial Rational
+rootRecip :: (Fractional k, Eq k) => UPolynomial k -> UPolynomial k
 rootRecip p = normalizePoly $ P.fromTerms [(c, P.var X `P.mpow` (d - P.deg xs)) | (c, xs) <- P.terms p]
   where
     d = P.deg p
 
--- 代数的数を係数とする多項式の根を、有理数係数多項式の根として表す
-rootSimpPoly :: (a -> UPolynomial Rational) -> UPolynomial a -> UPolynomial Rational
+-- | Given a polynomial P = Σ_i a_i x^i over L/K where each a_i is a root of polynomial @f a_i@ over K,
+-- @rootSimpPoly f P@ computes a polynomial Q over K such that roots of P is a subset of roots of Q.
+rootSimpPoly :: forall k l. (Fractional k, Ord k) => (l -> UPolynomial k) -> UPolynomial l -> UPolynomial k
 rootSimpPoly f p = findPoly (P.var 0) ps
   where
-    ys :: [(UPolynomial Rational, Var)]
+    ys :: [(UPolynomial k, Var)]
     ys = zip (Set.toAscList $ Set.fromList [f c | (c, _) <- P.terms p]) [1..]
 
-    m :: Map (UPolynomial Rational) Var
+    m :: Map (UPolynomial k) Var
     m = Map.fromDistinctAscList ys
 
-    p' :: Polynomial Rational Var
+    p' :: Polynomial k Var
     p' = P.eval (\X -> P.var 0) (P.mapCoeff (\c -> P.var (m Map.! (f c))) p)
 
-    ps :: [Polynomial Rational Var]
+    ps :: [Polynomial k Var]
     ps = p' : [P.subst q (\X -> P.var x) | (q, x) <- ys]
 
-rootNthRoot :: Integer -> UPolynomial Rational -> UPolynomial Rational
+rootNthRoot :: (Fractional k, Ord k) => Integer -> UPolynomial k -> UPolynomial k
 rootNthRoot n p = P.subst p (\X -> (P.var X)^n)
 
-lift2 :: (forall a. Num a => a -> a -> a)
-      -> UPolynomial Rational -> UPolynomial Rational -> UPolynomial Rational
+lift2 :: forall k. (Fractional k, Ord k) => (forall a. Num a => a -> a -> a)
+      -> UPolynomial k -> UPolynomial k -> UPolynomial k
 lift2 f p1 p2 = findPoly f_a_b gbase
   where
     a, b :: Var
     a = 0
     b = 1
 
-    f_a_b :: Polynomial Rational Var
+    f_a_b :: Polynomial k Var
     f_a_b = f (P.var a) (P.var b)
 
-    gbase :: [Polynomial Rational Var]
+    gbase :: [Polynomial k Var]
     gbase = [ P.subst p1 (\X -> P.var a), P.subst p2 (\X -> P.var b) ]              
 
--- ps のもとで c を根とする多項式を求める
-findPoly :: Polynomial Rational Var -> [Polynomial Rational Var] -> UPolynomial Rational
+-- | Given a polynomial P and polynomials {P1,…,Pn} over K,
+-- findPoly P [P1..Pn] computes a non-zero polynomial Q such that Q[P] = 0 modulo {P1,…,Pn}.
+findPoly :: forall k. (Fractional k, Ord k) => Polynomial k Var -> [Polynomial k Var] -> UPolynomial k
 findPoly c ps = normalizePoly $ sum [P.constant coeff * (P.var X) ^ n | (n,coeff) <- zip [0..] coeffs]
   where  
     vn :: Var
@@ -98,14 +100,14 @@ findPoly c ps = normalizePoly $ sum [P.constant coeff * (P.var X) ^ n | (n,coeff
       where
         vs = Set.fromList [x | p <- (c:ps), (_,xs) <- P.terms p, (x,_) <- P.mindices xs]
 
-    coeffs :: [Rational]
+    coeffs :: [k]
     coeffs = head $ catMaybes $ [isLinearlyDependent cs2 | cs2 <- inits cs]
       where
         cmp = P.grlex
         ps' = GB.basis cmp ps
         cs  = iterate (\p -> P.reduce cmp (c * p) ps') 1
 
-    isLinearlyDependent :: [Polynomial Rational Var] -> Maybe [Rational]
+    isLinearlyDependent :: [Polynomial k Var] -> Maybe [k]
     isLinearlyDependent cs = if any (0/=) sol then Just sol else Nothing
       where
         cs2 = zip [vn..] cs
