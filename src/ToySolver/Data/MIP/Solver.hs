c57d25ab2310e556eec65e58f6bf6c0f45c31bef
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-01-15 14:47:33 +0900

    :sparkles: add ToySolver.Data.MIP.Solver.CPLEX module

--- src/ToySolver/Data/MIP/Solver.hs
+++ src/ToySolver/Data/MIP/Solver.hs
@@ -2,6 +2,7 @@
 module ToySolver.Data.MIP.Solver
   ( module ToySolver.Data.MIP.Solver.Base
   , module ToySolver.Data.MIP.Solver.CBC
+  , module ToySolver.Data.MIP.Solver.CPLEX
   , module ToySolver.Data.MIP.Solver.Glpsol
   , module ToySolver.Data.MIP.Solver.GurobiCl
   , module ToySolver.Data.MIP.Solver.LPSolve
@@ -10,6 +11,7 @@ module ToySolver.Data.MIP.Solver
 
 import ToySolver.Data.MIP.Solver.Base
 import ToySolver.Data.MIP.Solver.CBC
+import ToySolver.Data.MIP.Solver.CPLEX
 import ToySolver.Data.MIP.Solver.Glpsol
 import ToySolver.Data.MIP.Solver.GurobiCl
 import ToySolver.Data.MIP.Solver.LPSolve
