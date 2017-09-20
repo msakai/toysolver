b88b0ca114405c01739b9b00ceeb49d88c1c4207
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-09-21 00:58:44 +0900

    rewrite build_bdist_linux.sh to use stack

--- build_bdist_linux.sh
+++ build_bdist_linux.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
-export CABALVER=1.22
-export GHCVER=7.10.3
 
-sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get update
+STACK_YAML=stack.yaml
+RESOLVER=lts-9.2
 
-sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER
-export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:~/.cabal/bin:$PATH
+# wget -qO- https://get.haskellstack.org/ | sh
+PATH=$HOME/.local/bin:$PATH
 
-sudo apt-get install happy-1.19.4 alex-3.1.3
-export PATH=/opt/alex/3.1.3/bin:/opt/happy/1.19.4/bin:$PATH
+stack --stack-yaml=$STACK_YAML --resolver=$RESOLVER --install-ghc build \
+  --flag toysolver:BuildToyFMF \
+  --flag toysolver:BuildSamplePrograms
 
-cabal sandbox init
-cabal update
-cabal install --only-dependencies --flag=BuildToyFMF --flag=BuildSamplePrograms --flag=BuildMiscPrograms
-#cabal configure --disable-shared --ghc-options="-static -optl-static -optl-pthread" --flag=BuildToyFMF --flag=BuildSamplePrograms --flag=BuildMiscPrograms
-cabal configure -fLinuxStatic -f-UseHaskeline --flag=BuildToyFMF --flag=BuildSamplePrograms --flag=BuildMiscPrograms
-cabal build
-
-VER=`ghc -ignore-dot-ghci -e ":m + Control.Monad Distribution.Package Distribution.PackageDescription Distribution.PackageDescription.Parse Distribution.Verbosity Data.Version" -e 'putStrLn =<< liftM (showVersion . pkgVersion . package . packageDescription) (readPackageDescription silent "toysolver.cabal")'`
-OS=`ghc -ignore-dot-ghci -e ":m +System.Info" -e "putStrLn os"`
-ARCH=`ghc -ignore-dot-ghci -e ":m +System.Info" -e "putStrLn arch"`
+VER=`stack exec ghc -- -ignore-dot-ghci -e ":m + Control.Monad Distribution.Package Distribution.PackageDescription Distribution.PackageDescription.Parse Distribution.Verbosity Data.Version" -e 'putStrLn =<< liftM (showVersion . pkgVersion . package . packageDescription) (readPackageDescription silent "toysolver.cabal")'`
+STACK_LOCAL_INSTALL_ROOT=`stack --stack-yaml=$STACK_YAML --resolver=$RESOLVER path --local-install-root`
+OS=`stack exec ghc -- -ignore-dot-ghci -e ":m +System.Info" -e "putStrLn os"`
+ARCH=`stack exec ghc -- -ignore-dot-ghci -e ":m +System.Info" -e "putStrLn arch"`
+PLATFORM=$ARCH-$OS
 
 PKG=toysolver-${VER}-${OS}-${ARCH}
 
 rm -r $PKG
 mkdir $PKG
 mkdir $PKG/bin
-cp dist/build/htc/htc dist/build/knapsack/knapsack dist/build/nonogram/nonogram dist/build/nqueens/nqueens dist/build/toyconvert/toyconvert dist/build/sudoku/sudoku dist/build/toyfmf/toyfmf dist/build/toysat/toysat dist/build/toysmt/toysmt dist/build/toysolver/toysolver $PKG/bin
-strip $PKG/bin/*
+cp $STACK_LOCAL_INSTALL_ROOT/bin/{toyconvert,toyfmf,toyqbf,toysat,toysmt,toysolver} $PKG/bin/
+cp $STACK_LOCAL_INSTALL_ROOT/bin/{assign,htc,knapsack,nonogram,nqueens,numberlink,sudoku} $PKG/bin/
 cp -a samples $PKG/
 cp COPYING-GPL README.md CHANGELOG.markdown $PKG/
 tar Jcf $PKG.tar.xz $PKG --owner=sakai --group=sakai
