e869c05b8d8dffa9d980d482e951ae75eca19a48
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-10-08 23:11:35 +0900

    ToySolver.Arith.DifferenceLogic: improve API and documention

--- src/ToySolver/Arith/DifferenceLogic.hs
+++ src/ToySolver/Arith/DifferenceLogic.hs
@@ -17,7 +17,8 @@
 --
 -----------------------------------------------------------------------------
 module ToySolver.Arith.DifferenceLogic
-  ( SimpleAtom
+  ( SimpleAtom (..)
+  , Diff (..)
   , solve
   ) where
 
@@ -30,9 +31,22 @@ import Data.Monoid
 
 import ToySolver.Graph.ShortestPath (bellmanFord, lastInEdge, bellmanFordDetectNegativeCycle, monoid')
 
--- (a,b,k) represents (a - b ≤ k)
-type SimpleAtom v b = (v,v,b)
+infixl 6 :-
+infix 4 :<=
 
+-- | Difference of two variables
+data Diff v = v :- v
+  deriving (Eq, Ord, Show)
+
+-- | @a :- b :<= k@ represents /a - b ≤ k/
+data SimpleAtom v b = Diff v :<= b
+  deriving (Eq, Ord, Show)
+
+-- | Takes labeled list of constraints, and returns either
+--
+-- * unsatisfiable set of constraints as a set of labels, or
+--
+-- * satisfying assignment.
 solve
   :: (Hashable label, Eq label, Hashable v, Eq v, Real b)
   => [(label, SimpleAtom v b)]
@@ -42,18 +56,18 @@ solve xs =
     Just f -> Left $ HashSet.fromList $ appEndo f []
     Nothing -> Right $ fmap (\(c,_) -> - c) d
   where
-    vs = HashSet.toList $ HashSet.fromList [v | (_,(a,b,_)) <- xs, v <- [a,b]]
-    g = HashMap.fromList [(a,[(b,k,l)]) | (l,(a,b,k)) <- xs]
+    vs = HashSet.toList $ HashSet.fromList [v | (_,(a :- b :<= _)) <- xs, v <- [a,b]]
+    g = HashMap.fromList [(a,[(b,k,l)]) | (l,(a :- b :<= k)) <- xs]
     d = bellmanFord lastInEdge g vs
 
 -- M = {a−b ≤ 2, b−c ≤ 3, c−a ≤ −3}
 test_sat = solve xs
   where
     xs :: [(Int, SimpleAtom Char Int)]
-    xs = [(1, ('a','b',2)), (2, ('b','c',3)), (3, ('c','a',-3))]
+    xs = [(1, ('a' :- 'b' :<= 2)), (2, ('b' :- 'c' :<= 3)), (3, ('c' :- 'a' :<= -3))]
 
 -- M = {a−b ≤ 2, b−c ≤ 3, c−a ≤ −7}
 test_unsat = solve xs
   where
     xs :: [(Int, SimpleAtom Char Int)]
-    xs = [(1, ('a','b',2)), (2, ('b','c',3)), (3, ('c','a',-7))]
+    xs = [(1, ('a' :- 'b' :<= 2)), (2, ('b' :- 'c' :<= 3)), (3, ('c' :- 'a' :<= -7))]
