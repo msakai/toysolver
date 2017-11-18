f8190b36bc651c2fc92c3dfddb4b006cfe256eb1
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-11-18 13:27:39 +0900

    toysmt: switch from System.Console.GetOpt to optparse-applicative

--- app/toysmt/toysmt.hs
+++ app/toysmt/toysmt.hs
@@ -13,19 +13,19 @@
 -----------------------------------------------------------------------------
 module Main where
 
-import Control.Applicative ((<*))
+import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans
-import Data.Default.Class
+import Data.Monoid
 import Data.Version
-import System.Console.GetOpt
+import Options.Applicative hiding (Parser)
+import qualified Options.Applicative as Opt
 #ifdef USE_HASKELINE_PACKAGE
 import qualified System.Console.Haskeline as Haskeline
 #endif
-import System.Environment
 import System.Exit
 import System.IO
-import Text.Parsec
+import Text.Parsec hiding (many)
 import Text.Parsec.String
 
 import Smtlib.Parsers.CommandsParsers
@@ -42,51 +42,49 @@ data Mode
 
 data Options
   = Options
-  { optMode :: Maybe Mode
-  , optInteractive :: Bool
+  { optInteractive :: Bool
+  , optFiles :: [FilePath]
   }
 
-instance Default Options where
-  def =
-    Options
-    { optMode = Nothing
-    , optInteractive = False 
-    }
+optionsParser :: Opt.Parser Options
+optionsParser = Options
+  <$> interactiveOption
+  <*> fileArgs
+  where
+    interactiveOption :: Opt.Parser Bool
+    interactiveOption = switch
+      $  long "interactive"
+      <> help "force interactive mode"
 
-options :: [OptDescr (Options -> Options)]
-options =
-    [ Option ['h'] ["help"]   (NoArg (\opt -> opt{ optMode = Just ModeHelp   })) "show help"
-    , Option [] ["version"]   (NoArg (\opt -> opt{ optMode = Just ModeVersion})) "show version"
-    , Option [] ["interactive"] (NoArg (\opt -> opt{ optMode = Just ModeInteractive })) "force interactive mode"
-    ]
+    fileArgs :: Opt.Parser [FilePath]
+    fileArgs = many $ strArgument $ metavar "FILE"
+
+parserInfo :: ParserInfo Options
+parserInfo = info (helper <*> versionOption <*> optionsParser)
+  $  fullDesc
+  <> header "toysmt - a SMT solver"
+  where
+    versionOption :: Opt.Parser (a -> a)
+    versionOption = infoOption (showVersion version)
+      $  hidden
+      <> long "version"
+      <> help "Show version"
 
 main :: IO ()
 main = do
 #ifdef FORCE_CHAR8
   setEncodingChar8
 #endif
-
-  args <- getArgs
-  case getOpt Permute options args of
-    (_,_,errs@(_:_)) -> do
-      mapM_ putStrLn errs
-      exitFailure
-
-    (o,args2,[]) -> do
-      let opt = foldl (flip id) def o
-      case optMode opt of
-        Just ModeHelp -> showHelp stdout
-        Just ModeVersion -> hPutStrLn stdout (showVersion version)
-        Just ModeInteractive -> do
-          solver <- newSolver
-          mapM_ (loadFile solver) args2
-          repl solver          
-        Nothing -> do
-          solver <- newSolver
-          if null args2 then
-            repl solver
-          else
-            mapM_ (loadFile solver) args2
+  opt <- execParser parserInfo
+  solver <- newSolver
+  if optInteractive opt then do
+    mapM_ (loadFile solver) (optFiles opt)
+    repl solver        
+  else do
+    if null (optFiles opt) then
+      repl solver
+    else
+      mapM_ (loadFile solver) (optFiles opt)
 
 loadFile :: Solver -> FilePath -> IO ()
 loadFile solver fname = do
@@ -135,14 +133,3 @@ replHaskeline solver = Haskeline.runInputT Haskeline.defaultSettings $ forever $
           lift $ execCommand solver cmd
 
 #endif
-
-showHelp :: Handle -> IO ()
-showHelp h = hPutStrLn h (usageInfo header options)
-
-header :: String
-header = unlines
-  [ "Usage:"
-  , "  toysmt [OPTION]... [file.smt2]"
-  , ""
-  , "Options:"
-  ]
