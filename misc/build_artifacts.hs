4737f66be188037bdf458223a6c123edce290341
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-02-01 00:18:30 +0900

    build_artifacts.hs: use .tar.xz instead of .zip on macos

--- misc/build_artifacts.hs
+++ misc/build_artifacts.hs
@@ -20,7 +20,7 @@ main = sh $ do
         case Info.os of
           "mingw32" -> (if Info.arch == "x86_64" then "win64" else "win32", Just "exe", archive7z)
           "linux"   -> ("linux-" ++ Info.arch, Nothing, archiveTarXz)
-          "darwin"  -> ("macos", Nothing, archiveZip)
+          "darwin"  -> ("macos", Nothing, archiveTarXz)
           _ -> error ("unknown os: " ++ Info.os)
       exe_files =
         [ "toyconvert"
