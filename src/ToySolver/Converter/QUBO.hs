75c03593ed67180f88a38a3ba84eb72511800f3a
Masahiro Sakai <masahiro.sakai@gmail.com>
2018-11-21 12:53:28 +0900

    add IsingModel and QUBO↔IsingModel converter

--- src/ToySolver/Converter/QUBO.hs
+++ src/ToySolver/Converter/QUBO.hs
@@ -16,7 +16,13 @@ module ToySolver.Converter.QUBO
   , QUBO2PBOInfo (..)
 
   , pboAsQUBO
-  , PBOAsQUBOInfo
+  , PBOAsQUBOInfo (..)
+
+  , qubo2ising
+  , QUBO2IsingInfo (..)
+
+  , ising2qubo
+  , Ising2QUBOInfo (..)
   ) where
 
 import Control.Monad
@@ -24,6 +30,7 @@ import Control.Monad.State
 import Data.Array.Unboxed
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
+import Data.List
 import Data.Maybe
 import qualified Data.PseudoBoolean as PBFile
 import ToySolver.Converter.Base
@@ -104,6 +111,135 @@ instance BackwardTransformer PBOAsQUBOInfo where
   transformBackward (PBOAsQUBOInfo _offset) sol = ixmap (lb+1,ub+1) (subtract 1) sol
     where
       (lb,ub) = bounds sol
+
+-- -----------------------------------------------------------------------------
+
+qubo2ising :: (Eq a, Num a) => QUBO.Problem a -> (QUBO.IsingModel a, QUBO2IsingInfo a)
+qubo2ising QUBO.Problem{ QUBO.quboNumVars = n, QUBO.quboMatrix = qq } =
+  ( QUBO.IsingModel
+    { QUBO.isingNumVars = n
+    , QUBO.isingInteraction = normalizeMat $ fmap (fmap negate) jj'
+    , QUBO.isingExternalMagneticField = normalizeVec $ fmap negate h'
+    }
+  , QUBO2IsingInfo c'
+  )
+  where
+    {-
+       Let xi = (si + 1)/2.
+
+       Then,
+         Qij xi xj
+       = Qij (si + 1)/2 (sj + 1)/2
+       = 1/4 Qij (si sj + si + sj + 1).
+
+       Also,
+         Qii xi xi
+       = Qii (si + 1)/2 (si + 1)/2
+       = 1/4 Qii (si si + 2 si + 1)
+       = 1/4 Qii (2 si + 2).
+    -}
+    (jj', h', c') = foldl' f (IntMap.empty, IntMap.empty, 0) $ do
+      (i, row)  <- IntMap.toList qq
+      (j, q_ij) <- IntMap.toList row
+      if i /= j then
+        return
+          ( IntMap.singleton (min i j) $ IntMap.singleton (max i j) q_ij
+          , IntMap.fromList [(i,q_ij), (j,q_ij)]
+          , q_ij
+          )
+      else
+        return
+          ( IntMap.empty
+          , IntMap.singleton i (2 * q_ij)
+          , 2 * q_ij
+          )
+
+    f (jj1, h1, c1) (jj2, h2, c2) =
+      ( IntMap.unionWith (IntMap.unionWith (+)) jj1 jj2
+      , IntMap.unionWith (+) h1 h2
+      , c1+c2
+      )
+
+data QUBO2IsingInfo a = QUBO2IsingInfo a
+
+instance Transformer (QUBO2IsingInfo a) where
+  type Source (QUBO2IsingInfo a) = QUBO.Solution
+  type Target (QUBO2IsingInfo a) = QUBO.Solution
+
+instance ForwardTransformer (QUBO2IsingInfo a) where
+  transformForward _ sol = sol
+
+instance BackwardTransformer (QUBO2IsingInfo a) where
+  transformBackward _ sol = sol
+
+instance ObjValueTransformer (QUBO2IsingInfo a) where
+  type SourceObjValue (QUBO2IsingInfo a) = a
+  type TargetObjValue (QUBO2IsingInfo a) = a
+
+instance Num a => ObjValueForwardTransformer (QUBO2IsingInfo a) where
+  transformObjValueForward (QUBO2IsingInfo offset) obj = 4 * obj - offset
+
+-- XXX
+instance Fractional a => ObjValueBackwardTransformer (QUBO2IsingInfo a) where
+  transformObjValueBackward (QUBO2IsingInfo offset) obj = (obj + offset) / 4
+
+-- -----------------------------------------------------------------------------
+
+ising2qubo :: (Eq a, Num a) => QUBO.IsingModel a -> (QUBO.Problem a, Ising2QUBOInfo a)
+ising2qubo QUBO.IsingModel{ QUBO.isingNumVars = n, QUBO.isingInteraction = jj, QUBO.isingExternalMagneticField = h } =
+  ( QUBO.Problem
+    { QUBO.quboNumVars = n
+    , QUBO.quboMatrix = mkMat m
+    }
+  , Ising2QUBOInfo offset
+  )
+  where
+    {-
+       Let si = 2 xi - 1
+
+       Then,
+         - Jij si sj
+       = - Jij (2 xi - 1) (2 xj - 1)
+       = - Jij (4 xi xj - 2 xi - 2 xj + 1)
+       = - 4 Jij xi xj + 2 Jij xi + 2 Jij xj - Jij
+       = - 4 Jij xi xj + 2 Jij xi xi + 2 Jij xj xj - Jij
+
+         - hi si
+       = - hi (2 xi - 1)
+       = - 2 hi xi + hi
+       = - 2 hi xi xi + hi
+    -}
+    m =
+      concat
+      [ [(i, j, -4 * jj_ij), (i, i,  2 * jj_ij), (j, j,  2 * jj_ij)]
+      | (i, row) <- IntMap.toList jj, (j, jj_ij) <- IntMap.toList row
+      ] ++
+      [ (i, i,  -2 * hi) | (i, hi) <- IntMap.toList h ]
+    offset =
+      - sum [jj_ij | row <- IntMap.elems jj, jj_ij <- IntMap.elems row]
+      + sum (IntMap.elems h)
+
+data Ising2QUBOInfo a = Ising2QUBOInfo a
+
+instance Transformer (Ising2QUBOInfo a) where
+  type Source (Ising2QUBOInfo a) = QUBO.Solution
+  type Target (Ising2QUBOInfo a) = QUBO.Solution
+
+instance ForwardTransformer (Ising2QUBOInfo a) where
+  transformForward _ sol = sol
+
+instance BackwardTransformer (Ising2QUBOInfo a) where
+  transformBackward _ sol = sol
+
+instance ObjValueTransformer (Ising2QUBOInfo a) where
+  type SourceObjValue (Ising2QUBOInfo a) = a
+  type TargetObjValue (Ising2QUBOInfo a) = a
+
+instance Num a => ObjValueForwardTransformer (Ising2QUBOInfo a) where
+  transformObjValueForward (Ising2QUBOInfo offset) obj = obj - offset
+
+instance Num a => ObjValueBackwardTransformer (Ising2QUBOInfo a) where
+  transformObjValueBackward (Ising2QUBOInfo offset) obj = obj + offset
 
 -- -----------------------------------------------------------------------------
 
--- src/ToySolver/QUBO.hs
+++ src/ToySolver/QUBO.hs
@@ -11,9 +11,14 @@
 -- 
 -----------------------------------------------------------------------------
 module ToySolver.QUBO
-  ( Problem (..)
+  ( -- * QUBO (quadratic unconstrained boolean optimization)
+    Problem (..)
   , Solution (..)
   , eval
+
+    -- * Ising Model
+  , IsingModel (..)
+  , evalIsingModel
   ) where
 
 import Control.Monad
@@ -39,3 +44,25 @@ eval sol prob = sum $ do
   (x2, c) <- IntMap.toList row
   guard $ sol ! x2
   return c
+
+
+data IsingModel a
+  = IsingModel
+  { isingNumVars :: !Int
+    -- ^ Number of variables N. Variables are numbered from 0 to N-1.
+  , isingInteraction :: IntMap (IntMap a)
+    -- ^ Interaction \(J_{i,j}\) with \(i < j\).
+  , isingExternalMagneticField :: IntMap a
+    -- ^ External magnetic field \(h_j\).
+  }
+  deriving (Eq, Show)
+
+-- | +1 is represented as True and -1 is represented as False.
+evalIsingModel :: Num a => Solution -> IsingModel a -> a
+evalIsingModel sol m =
+  - sum [ jj_ij * sigma i *  sigma j
+        | (i, row) <- IntMap.toList $ isingInteraction m, (j, jj_ij) <- IntMap.toList row
+        ]
+  - sum [ h_i * sigma i | (i, h_i) <- IntMap.toList $ isingExternalMagneticField m ]
+  where
+    sigma i = if sol ! i then 1 else -1
