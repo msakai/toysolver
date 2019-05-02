f2efe3a9071f1b8bc1237c5c3c200697495848e0
Masahiro Sakai <masahiro.sakai@gmail.com>
2019-05-03 00:06:55 +0900

    add SequentialCounter as a method for encoding cardinality constraints

--- src/ToySolver/SAT/Encoder/Cardinality.hs
+++ src/ToySolver/SAT/Encoder/Cardinality.hs
@@ -26,6 +26,7 @@ import qualified ToySolver.SAT.Types as SAT
 import qualified ToySolver.SAT.Encoder.Tseitin as Tseitin
 import ToySolver.SAT.Encoder.Cardinality.Internal.Naive
 import ToySolver.SAT.Encoder.Cardinality.Internal.ParallelCounter
+import ToySolver.SAT.Encoder.PB.Internal.BDD as BDD
 
 -- -------------------------------------------------------------------
 
@@ -33,8 +34,18 @@ data Encoder m = Encoder (Tseitin.Encoder m) Strategy
 
 data Strategy
   = Naive
+  | SequentialCounter
   | ParallelCounter
   deriving (Show, Eq, Ord, Enum, Bounded)
+{-
+"Sequential Counter" from "Towards an Optimal CNF Encoding of Boolean
+Cardinality Constraints" is a special case of BDD-based encoding of
+"Translating Pseudo-Boolean Constraints into SAT" (using the fact C→B
+to represent ite(A,B,C) as (A∧B)∨C instead of (A∧B)∨(¬A∧C))?
+
+http://www.carstensinz.de/papers/CP-2005.pdf
+http://www.st.ewi.tudelft.nl/jsat/content/volume2/JSAT2_1_Een.pdf
+-}
 
 newEncoder :: Monad m => Tseitin.Encoder m -> m (Encoder m)
 newEncoder tseitin = return $ Encoder tseitin ParallelCounter
@@ -60,9 +71,11 @@ instance PrimMonad m => SAT.AddCardinality m (Encoder m) where
         case strategy of
           Naive -> addAtLeastNaive tseitin (lhs,rhs)
           ParallelCounter -> addAtLeastParallelCounter tseitin (lhs,rhs)
+          SequentialCounter -> BDD.addPBLinAtLeastBDD tseitin ([(1,l) | l <- lhs], fromIntegral rhs)
 
 encodeAtLeast :: PrimMonad m => Encoder m -> SAT.AtLeast -> m SAT.Lit
 encodeAtLeast (Encoder tseitin strategy) =
   case strategy of
     Naive -> encodeAtLeastNaive tseitin
     ParallelCounter -> encodeAtLeastParallelCounter tseitin
+    SequentialCounter -> \(lhs,rhs) -> BDD.encodePBLinAtLeastBDD tseitin ([(1,l) | l <- lhs], fromIntegral rhs)
