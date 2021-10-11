ff5f7bc37e513c57f23b8b1e6900f43641276c99
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-10-11 17:22:57 +0900

    update misc/remove-trailing-space.rb

--- misc/remove-trailing-space.rb
+++ misc/remove-trailing-space.rb
@@ -3,7 +3,7 @@ require 'pathname'
 base = ARGV[0] || "."
 
 Dir.glob("**/*.hs", base: base){|fname|
-  s = File.read(File.join(base, fname))
+  s = File.read(File.join(base, fname), encoding: "utf-8")
   if s.gsub!(/[ \t]+$/, "")
     puts fname
     File.write(File.join(base, fname), s)
