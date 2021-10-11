c9c57f80331f93af3312620f253d99f8f76957eb
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-10-11 17:22:10 +0900

    update misc/collect-language-pragmas.rb

--- misc/collect-language-pragmas.rb
+++ misc/collect-language-pragmas.rb
@@ -1,15 +1,20 @@
 require 'pathname'
 
 base = ARGV[0] || "."
-exts = []
+exts = {}
 
 Dir.glob("**/*.hs", base: base){|fname|
-  src = File.read(File.join(base, fname))
+  src = File.read(File.join(base, fname), encoding: "utf-8")
   src.scan(/{-# LANGUAGE([^#]*)#-}/){
-    exts.concat($1.strip().split(/\s*,\s*/))
+    $1.strip().split(/\s*,\s*/).each{|ext|
+      (exts[ext] ||= []).push fname
+    }
   }
 }
 
-exts.sort.uniq.each{|ext|
+exts.keys.sort.each{|ext|
   puts ext
+  exts[ext].each{|fname|
+    puts "  #{fname}"
+  }
 }
