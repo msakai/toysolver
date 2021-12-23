49e98ed672b5a5775ee9ed146847068dde0d2bcf
Masahiro Sakai <masahiro.sakai@gmail.com>
2021-12-23 23:34:14 +0900

    add encodePBLinAtLeastBDD' :: PrimMonad m => Tseitin.Encoder m -> SAT.PBLinAtLeast -> m Tseitin.Formula

--- src/ToySolver/SAT/Encoder/PB/Internal/BDD.hs
+++ src/ToySolver/SAT/Encoder/PB/Internal/BDD.hs
@@ -21,6 +21,7 @@
 module ToySolver.SAT.Encoder.PB.Internal.BDD
   ( addPBLinAtLeastBDD
   , encodePBLinAtLeastBDD
+  , encodePBLinAtLeastBDD'
   ) where
 
 import Control.Monad.State.Strict
@@ -29,38 +30,44 @@ import Data.Ord
 import Data.List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import ToySolver.Data.Boolean
 import qualified ToySolver.SAT.Types as SAT
 import qualified ToySolver.SAT.Encoder.Tseitin as Tseitin
 
 addPBLinAtLeastBDD :: PrimMonad m => Tseitin.Encoder m -> SAT.PBLinAtLeast -> m ()
 addPBLinAtLeastBDD enc constr = do
-  l <- encodePBLinAtLeastBDD enc constr
-  SAT.addClause enc [l]
+  formula <- encodePBLinAtLeastBDD' enc constr
+  Tseitin.addFormula enc formula
 
 encodePBLinAtLeastBDD :: forall m. PrimMonad m => Tseitin.Encoder m -> SAT.PBLinAtLeast -> m SAT.Lit
-encodePBLinAtLeastBDD enc (lhs,rhs) = do
+encodePBLinAtLeastBDD enc constr = do
+  formula <- encodePBLinAtLeastBDD' enc constr
+  Tseitin.encodeFormula enc formula
+
+encodePBLinAtLeastBDD' :: forall m. PrimMonad m => Tseitin.Encoder m -> SAT.PBLinAtLeast -> m Tseitin.Formula
+encodePBLinAtLeastBDD' _enc (lhs,rhs) = do
   let lhs' = sortBy (flip (comparing fst)) lhs
   flip evalStateT Map.empty $ do
-    let f :: SAT.PBLinSum -> Integer -> Integer -> StateT (Map (SAT.PBLinSum, Integer) SAT.Lit) m SAT.Lit
+    let f :: SAT.PBLinSum -> Integer -> Integer -> StateT (Map (SAT.PBLinSum, Integer) Tseitin.Formula) m Tseitin.Formula
         f xs rhs slack
-          | rhs <= 0  = lift $ Tseitin.encodeConj enc [] -- true
-          | slack < 0 = lift $ Tseitin.encodeDisj enc [] -- false
+          | rhs <= 0  = return true
+          | slack < 0 = return false
           | otherwise = do
               m <- get
               case Map.lookup (xs,rhs) m of
-                Just l -> return l
+                Just formula -> return formula
                 Nothing -> do
                   case xs of
                     [] -> error "encodePBLinAtLeastBDD: should not happen"
-                    [(_,l)] -> return l
+                    [(_,l)] -> return $ Tseitin.Atom l
                     (c,l) : xs' -> do
-                      thenLit <- f xs' (rhs - c) slack
-                      l2 <- lift $ Tseitin.encodeConjWithPolarity enc Tseitin.polarityPos [l, thenLit]
-                      l3 <- if c > slack then
-                              return l2
+                      thenFormula <- f xs' (rhs - c) slack
+                      let f2 = Tseitin.Atom l .&&. thenFormula
+                      f3 <- if c > slack then
+                              return f2
                             else do
-                              elseLit <- f xs' rhs (slack - c)
-                              lift $ Tseitin.encodeDisjWithPolarity enc Tseitin.polarityPos [l2, elseLit]
-                      modify (Map.insert (xs,rhs) l3)
-                      return l3
+                              elseFormula <- f xs' rhs (slack - c)
+                              return $ f2 .||. elseFormula
+                      modify (Map.insert (xs,rhs) f3)
+                      return f3
     f lhs' rhs (sum [c | (c,_) <- lhs'] - rhs)
