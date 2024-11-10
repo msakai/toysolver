736e7342c441e533f7482883a8e0b30a1a076bc4
Masahiro Sakai <masahiro.sakai@gmail.com>
2024-11-10 20:20:28 +0900

    improve misc/collect-min-version-macro.rb

--- misc/collect-min-version-macro.rb
+++ misc/collect-min-version-macro.rb
@@ -2,18 +2,21 @@ require 'pathname'
 
 base = ARGV[0] || "."
 
-a = []
+h = {}
 
 Dir.glob("**/*.hs", base: base){|fname|
   File.open(fname, encoding: "utf-8"){|f|
     f.each_line{|l|
       if /MIN_VERSION_\w+\(\d+,\d+,\d+\)/ =~ l
-        a << $~[0]
+        (h[$~[0]] ||= []) << fname
       end
     }
   }
 }
 
-a.sort.uniq.each{|l|
-  puts l
+h.keys.sort.each {|k|
+  puts k
+  h[k].each {|fname|
+    puts "  #{fname}"
+  }
 }
