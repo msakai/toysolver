89948e7c048b7abb7796c684ebd2f3abfc57fd01
Masahiro Sakai <masahiro.sakai@gmail.com>
2025-02-09 00:37:34 +0900

    change arbitrarySimpleGraph to take the number of vertexes

--- test/Test/Graph.hs
+++ test/Test/Graph.hs
@@ -17,14 +17,13 @@ import ToySolver.Graph.Base
 
 -- ------------------------------------------------------------------------
 
-arbitrarySimpleGraph :: Gen Graph
-arbitrarySimpleGraph = do
-  sized $ \n -> do
-    m <- choose (0, n*n-1)
-    fmap (graphFromUnorderedEdges n) $ replicateM m $ do
-      node1 <- choose (0, n-1)
-      node2 <- fmap (\i -> (node1 + i) `mod` n) $ choose (1, n-1)
-      return (node1, node2, ())
+arbitrarySimpleGraph :: Int -> Gen Graph
+arbitrarySimpleGraph n = do
+  m <- choose (0, n*n-1)
+  fmap (graphFromUnorderedEdges n) $ replicateM m $ do
+    node1 <- choose (0, n-1)
+    node2 <- fmap (\i -> (node1 + i) `mod` n) $ choose (1, n-1)
+    return (node1, node2, ())
 
 vertexes :: EdgeLabeledGraph a -> IntSet
 vertexes = IntSet.fromAscList . uncurry enumFromTo . bounds
@@ -36,10 +35,11 @@ arbitrarySubset = fmap IntSet.fromAscList . sublistOf . IntSet.toAscList
 
 prop_indepndent_set_and_clique :: Property
 prop_indepndent_set_and_clique =
-  forAll arbitrarySimpleGraph $ \g ->
-    forAll (arbitrarySubset (vertexes g)) $ \s -> do
-      counterexample (show (graphToUnorderedEdges g)) $
-        s `isIndependentSetOf` g === s `isCliqueOf` complementSimpleGraph g
+  forAll arbitrary $ \(NonNegative n) -> do
+    forAll (arbitrarySimpleGraph n) $ \g ->
+      forAll (arbitrarySubset (vertexes g)) $ \s -> do
+        counterexample (show (graphToUnorderedEdges g)) $
+          s `isIndependentSetOf` g === s `isCliqueOf` complementSimpleGraph g
 
 -- ------------------------------------------------------------------------
 -- Test harness
