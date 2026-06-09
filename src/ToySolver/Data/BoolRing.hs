d8d6f8c5a46dc2cef45f4116a4402361e944127b
Masahiro Sakai <masahiro.sakai@gmail.com>
2026-06-09 21:33:34 +0900

    should use DeriveDataTypeable for deriving Data

--- src/ToySolver/Data/BoolRing.hs
+++ src/ToySolver/Data/BoolRing.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_HADDOCK show-extensions #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -----------------------------------------------------------------------------
 -- |
